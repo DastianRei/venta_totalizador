bcfbee9bcd600174fd646c075c1248658718fd31
Dayan <dayan.aguilar@ucb.edu.bo>
2023-02-18 15:56:41 -0400

    Mostrar Precio Por Item

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,9 @@
 const cantidadItems = document.querySelector("#cantidad-items");
 const div = document.querySelector("#cantidad-items-mostrar");
+const precioPorItem = document.querySelector("#precio-por-item");
 const formulario = document.querySelector("#totalizador-form");
 
 formulario.addEventListener("submit", (event) => {
-    event.preventDefault();
-    div.innerHTML = "<p>" + cantidadItems.value + "</p>";
-  });
-  
+  event.preventDefault();
+  div.innerHTML = "<p>" + cantidadItems.value + precioPorItem.value + "</p>";
+});
