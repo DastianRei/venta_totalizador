9adddb39955059b38dc1854d6ea52a8cbea4f85f
Dayan <dayan.aguilar@ucb.edu.bo>
2023-02-18 16:12:11 -0400

    Mostrar Estados a traves de la lista

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,11 @@
 const cantidadItems = document.querySelector("#cantidad-items");
 const div = document.querySelector("#cantidad-items-mostrar");
+const estados = document.querySelector("#estados");
 const precioPorItem = document.querySelector("#precio-por-item");
 const formulario = document.querySelector("#totalizador-form");
 
 formulario.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<p>" + cantidadItems.value + precioPorItem.value + "</p>";
+  div.innerHTML =
+    "<p>" + cantidadItems.value + precioPorItem.value + estados.value + "</p>";
 });
